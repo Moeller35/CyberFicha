3deeda3d39ed9e0f01782c7c1d17af7866030e49
Felipe Moeller <felipemoeller1921@gmail.com>
2019-10-23 21:46:52 -0300

    Reaproveitamento do header

--- CyberFicha/criar_conta.php
+++ CyberFicha/criar_conta.php
@@ -13,45 +13,8 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">    
-        <a class="navbar-brand" href="index.php">
-            <img src="images/icon.jpg" width="40" height="40" class="d-inline-block align-top" alt="">
-            CyberFicha
-        </a>
+    <?php include "php/header.php"?>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Ficha
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="create_sheet.php">Criar Ficha</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Minhas Fichas</a>
-                    </div>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Campanha
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="criar_campanha.php">Criar Campanha</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Minhas Campanhas</a>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="criar_conta.php">Criar conta</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="login.php">Login</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
     <form class="container formCriarConta" action="php/gravar_usuario.php" method="POST">
         <div class="form-group">
             <label for="nome">Nome Completo</label>
